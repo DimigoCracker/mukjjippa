a3bfed62247006603dd0e15ac28a411a80de17a3
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-02-16 13:03:34 +0900

    Add PlayerRight
    Add PlayerRight and ChangePlayerRIght Method

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool attackRight;    //공격권
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeAttackRight(bool attackRight)
+    {
+        this.attackRight = attackRight;
+    }
+}
